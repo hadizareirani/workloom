6b76020c855e11df80c1ca617e9a90163cdcb6bb
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2026-02-02 22:25:04 +0330

    feat: add TODO comments for implementing main app specific interception logic

--- src/shared/http/main-app.interceptor.ts
+++ src/shared/http/main-app.interceptor.ts
@@ -9,6 +9,8 @@ import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class MainAppInterceptor implements NestInterceptor {
+  // TODO: Implement main app specific interception logic
+  // TODO: Implement this class based on project requirements
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(tap(() => console.log(`After...`)));
   }
