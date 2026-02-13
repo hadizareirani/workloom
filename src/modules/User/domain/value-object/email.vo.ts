65e481aea84878c32615412cb2102ce31b922cb5
Hadi Zare Irani <22741265+hadizareirani@users.noreply.github.com>
2026-02-13 12:48:56 +0330

    feat: rename GUID comparison method and update fromString to fromPersistence; add Email value object with validation

---
+++ src/modules/User/domain/value-object/email.vo.ts
@@ -0,0 +1,26 @@
+export class Email {
+  private readonly _value: string;
+  private static readonly emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  private constructor(value: string) {
+    this._value = value;
+  }
+
+  static create(value: string): Email {
+    const isValid = this.emailRegex.test(value);
+    if (!isValid) throw new Error('Invalid email format');
+    return new Email(value);
+  }
+
+  static fromPersistence(email: string): Email {
+    return new Email(email);
+  }
+
+  compareTo(other: Email): boolean {
+    return this._value === other.value;
+  }
+
+  get value(): string {
+    return this._value;
+  }
+}
